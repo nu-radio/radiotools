1b9216e81e1960ae7141b01300b3eee4e8c11780
cg-laser <jcglaser@gmail.com>
2018-11-30 21:54:24 +0000

    add new transformation

--- radiotools/coordinatesystems.py
+++ radiotools/coordinatesystems.py
@@ -66,7 +66,7 @@ class cstrafo():
         e1 = np.array([st * cp, st * sp, ct])
         e2 = np.array([ct * cp, ct * sp, -st])
         e3 = np.array([-sp, cp, 0])
-        self.__transformation_matrix_onsky = np.matrix([e1, e2, e3])
+        self.__transformation_matrix_onsky = copy.copy(np.matrix([e1, e2, e3]))
         self.__inverse_transformation_matrix_onsky = np.linalg.inv(self.__transformation_matrix_onsky)
 
         # initilize transformation matrix from magnetic north to geographic north coordinate system
@@ -76,8 +76,17 @@ class cstrafo():
         e1 = np.array([c, -s, 0])
         e2 = np.array([s, c, 0])
         e3 = np.array([0, 0, 1])
-        self.__transformation_matrix_magnetic = np.matrix([e1, e2, e3])
+        self.__transformation_matrix_magnetic = copy.copy(np.matrix([e1, e2, e3]))
         self.__inverse_transformation_matrix_magnetic = np.linalg.inv(self.__transformation_matrix_magnetic)
+
+        # initilize transformation matrix from ground (geographic) cs to ground cs where x axis points into shower direction projected on ground
+        c = np.cos(-1 * azimuth)
+        s = np.sin(-1 * azimuth)
+        e1 = np.array([c, -s, 0])
+        e2 = np.array([s, c, 0])
+        e3 = np.array([0, 0, 1])
+        self.__transformation_matrix_azimuth = copy.copy(np.matrix([e1, e2, e3]))
+        self.__inverse_transformation_matrix_azimuth = np.linalg.inv(self.__transformation_matrix_azimuth)
 
 #     def __transform(self, positions, matrix):
 #         if(len(positions.shape) == 1):
@@ -104,11 +113,14 @@ class cstrafo():
     def transform_from_magnetic_to_geographic(self, positions):
         return self.__transform(positions, self.__transformation_matrix_magnetic)
 
-    def transform_from_magnetic_to_geographic2(self, positions):
-        return self.__transform2(positions, self.__transformation_matrix_magnetic)
-
     def transform_from_geographic_to_magnetic(self, positions):
         return self.__transform(positions, self.__inverse_transformation_matrix_magnetic)
+
+    def transform_from_azimuth_to_geographic(self, positions):
+        return self.__transform(positions, self.__transformation_matrix_azimuth)
+
+    def transform_from_geographic_to_azimuth(self, positions):
+        return self.__transform(positions, self.__inverse_transformation_matrix_azimuth)
 
     def transform_to_vxB_vxvxB(self, station_position, core=None):
         """ transform a single station position or a list of multiple

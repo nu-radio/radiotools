333362f969555fb28ca821b59e38da74b08d527f
Christian Glaser <christian.glaser@uci.edu>
2020-08-26 07:30:43 +0000

    add feldman cousins function

--- radiotools/helper.py
+++ radiotools/helper.py
@@ -868,3 +868,76 @@ def pretty_time_delta(seconds):
         return '%dm%ds' % (minutes, seconds)
     else:
         return '%ds' % (seconds,)
+
+
+def FC_limits(counts):
+    """
+    returns the 68%CL Feldman-Cousins limits for 0 background.
+    
+    Parameters
+    ----------
+    counts: float
+        the number of counts/events
+        
+    Returns tuple of floats
+        lower_bound, upper bound
+    """
+
+    from scipy.interpolate import interp1d
+
+    count_list = np.arange(0, 21)
+    lower_limits = [0.00,
+                    0.37,
+                    0.74,
+                    1.10,
+                    2.34,
+                    2.75,
+                    3.82,
+                    4.25,
+                    5.30,
+                    6.33,
+                    6.78,
+                    7.81,
+                    8.83,
+                    9.28,
+                    10.30,
+                    11.32,
+                    12.33,
+                    12.79,
+                    13.81,
+                    14.82,
+                    15.83]
+    upper_limits = [1.29,
+                    2.75,
+                    4.25,
+                    5.30,
+                    6.78,
+                    7, 81,
+                    9.28,
+                    10.30,
+                    11.32,
+                    12.79,
+                    13.81,
+                    14.82,
+                    16.29,
+                    17.30,
+                    18.32,
+                    19.32,
+                    20.80,
+                    21.81,
+                    22.82,
+                    25.30]
+
+    if counts > count_list[-1]:
+
+        return (counts - np.sqrt(counts), counts + np.sqrt(counts))
+
+    elif counts < 0:
+
+        return (0.00, 1.29)
+
+    low_interp = interp1d(count_list, lower_limits)
+    up_interp = interp1d(count_list, upper_limits)
+
+    return (low_interp(counts), up_interp(counts))
+

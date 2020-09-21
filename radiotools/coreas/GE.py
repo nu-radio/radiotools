f514906de3e1a74e287ef8dd926c1326e872de90
Christian Glaser <christian.glaser@uci.edu>
2020-09-21 09:12:00 +0000

    allow setting now email flags for GE submission

--- radiotools/coreas/GE.py
+++ radiotools/coreas/GE.py
@@ -13,7 +13,8 @@ def get_GE_preexecution(jobname=None, rundir=None,
     output.write("#$ -j y\n")
     output.write("#$ -V\n")
     output.write("#$ -q grb,grb64\n")
-    output.write("#$ -m {}\n".format(mailflags))
+    if(mailflags != ""):
+        output.write("#$ -m {}\n".format(mailflags))
     output.write("#$ -o {}\n".format(rundir))
     if(mpi):
         output.write("#$ -pe mpi {}\n".format(ncores))
